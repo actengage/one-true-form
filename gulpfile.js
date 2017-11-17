9ef42bb8eb766be4d01481464a1254dc4fced654
Justin Kimbrell <justin@objectivehtml.com>
2017-11-17 00:24:26 -0700

    Progress Update
    - Further restructured repo
    - Started breaking code into scss components
    - Incoporating mixings
    - Added Buttons and Grid components. Form component to come…

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
 
 gulp.task('sass', function () {
-    return gulp.src('./src/**/*.scss')
+    return gulp.src(['./src/main.scss'])
         .pipe(sass().on('error', sass.logError))
+        .pipe(rename('one-true-form.css'))
         .pipe(gulp.dest('./dist'));
 });
 
