36e70d732cc574fd6bf00a0224e2b253b6f27fe2
Justin Kimbrell <justin@objectivehtml.com>
2017-11-20 22:35:39 -0700

    Added minified version of CSS

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,15 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('./dist'));
 });
 
+gulp.task('sass:minify', function() {
+    gulp.src(['./src/main.scss'])
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(rename('one-true-form.min.css'))
+        .pipe(gulp.dest('./dist'));
+});
+
 gulp.task('sass:watch', function () {
     gulp.watch('./src/**/*.scss', ['sass']);
 });
